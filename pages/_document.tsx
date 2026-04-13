42326ec8a098c31d91b268bae49d192b05dae811
Viren-404 <bbhimani039@gmail.com>
2026-04-13 09:34:56 +0530

    add adsense code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3921457529361448"
-     crossorigin="anonymous"></script>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3921457529361448"
+          crossOrigin="anonymous"
+        />
       </Head>
       <body>
         <Main />
