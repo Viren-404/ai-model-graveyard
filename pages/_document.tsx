53b54b892f00608daed01b5338732b1ce7e556dc
Viren-404 <bbhimani039@gmail.com>
2026-04-13 09:29:35 +0530

    add adsense code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3921457529361448"
+     crossOrigin="anonymous"></script>
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
